8b458cc21252b3d0c4d51f97ed535e44a76472a3
ANJI-DESKTOP\flori <florian.harreau@outlook.com>
2018-01-27 11:43:18 +0100

    use pad controller instead of keybord

--- PJ/PJ.gd
+++ PJ/PJ.gd
@@ -1,60 +1,19 @@
 extends Node2D
 
-var acting = false
-var walking = false
-var walkingLeft = false
-var walkingRight = false
-
-var actionDuration = 0
-
-var velocity = 50
-
-var sprite
+var deadzone = 0.25
 
 func _ready():
-	sprite = get_node("Sprite/AnimatedSprite")
 	set_process(true)
 
 func _process(delta):
-	if !acting && Input.is_action_pressed("ui_select"):
-		acting = true
-		sprite.set_animation("action")
-		actionDuration = 0
-	elif acting:
-		actionDuration += delta
+	var sprite = self.get_node("Sprite")
 	
-	if actionDuration > 1:
-		acting = false
-		actionDuration = 0
-		sprite.set_animation("idle")
+	if Input.is_action_pressed("ui_select"):
+		sprite.start_acting()
 	
-	if !acting:
-		process_move(delta)
-
-func process_move(delta):
-	var zombie_pos = self.get_pos()
+	var direction = Vector2(Input.get_joy_axis(0,0), Input.get_joy_axis(0,1))
 	
-	if Input.is_action_pressed("ui_right"):
-		zombie_pos.x += velocity*delta
-		walking = true
-		walkingRight = true
-		walkingLeft = false
-	elif Input.is_action_pressed("ui_left"):
-		zombie_pos.x -= velocity*delta
-		walking = true
-		walkingRight = false
-		walkingLeft = true
+	if abs(direction.x) > deadzone || abs(direction.y) > deadzone:
+		sprite.move(direction.normalized(), delta)
 	else:
-		walking = false
-	
-	if walkingLeft:
-		self.set_scale(Vector2(-1,1))
-	elif walkingRight:
-		self.set_scale(Vector2(1,1))
-	
-	if walking:
-		sprite.set_animation("walk")
-	else:
-		sprite.set_animation("idle")
-	
-	self.set_pos(zombie_pos)
+		sprite.start_idle()
---
+++ Sprites/Sprite.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+var acting = false
+var walking = false
+
+var acting_since = 0
+
+var velocity = 50
+var last_direction = Vector2()
+
+onready var sprite = get_node("AnimatedSprite")
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if acting:
+		acting_since += delta
+	
+	if acting_since > 1:
+		acting = false
+		acting_since = 0
+		self.start_idle()
+
+func start_idle():
+	if !acting:
+		sprite.set_animation("idle")
+		walking = false
+
+func start_acting():
+	if !acting:
+		sprite.set_animation("action")
+		acting = true
+		walking = false
+
+func move(direction, delta):
+	if !acting:
+		var sprite = get_node("AnimatedSprite")
+		
+		if !walking:
+			sprite.set_animation("walk")
+			walking = true
+		
+		var current_pos = self.get_pos()
+		self.set_pos(current_pos + direction * velocity * delta)
+		
+		var scale = self.get_scale()
+		
+		if direction.x < 0:
+			scale.x = -1
+		elif direction.x > 0:
+			scale.x = 1
+		
+		self.set_scale(scale)
+		
+		last_direction = direction
