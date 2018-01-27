cefc182dbac465ed0073e4842d4d498f4dea3d5b
Laora Heintz <lola.mitou@gmail.com>
2018-01-27 22:29:41 +0100

    home_manette

--- Screen/Home.gd
+++ Screen/Home.gd
@@ -1,7 +1,14 @@
 extends Node
 
 func _ready():
-    get_node("LinkButton").connect("pressed",self,"_on_button_pressed")
+	get_node("LinkButton").connect("pressed",self,"_on_button_pressed")
+	#InputMap.action_add_event("click",InputEvent.JOYSTICK_BUTTON.
+	set_process_input(true)
+
+func _input(event):
+	if event.type == InputEvent.JOYSTICK_BUTTON && event.button_index == 0:
+		get_tree().change_scene("res://MainScene.tscn")
+
 
 func _on_button_pressed():
-    get_tree().change_scene("res://MainScene.tscn")
+	get_tree().change_scene("res://MainScene.tscn")
