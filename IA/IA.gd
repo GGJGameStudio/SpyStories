44eafa74217920779fb39252c8468068f7649c58
ANJI-DESKTOP\flori <florian.harreau@outlook.com>
2018-01-27 14:31:04 +0100

    spawning mob

---
+++ IA/IA.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var direction = Vector2(0,0)
+var duration = 1
+
+var acting_since = 0
+
+func _ready():
+	set_new_movement()
+	
+	set_process(true)
+
+func _process(delta):
+	acting_since += delta
+	
+	if acting_since > duration:
+		set_new_movement()
+	
+	if direction.length() > 0:
+		get_node("Sprite").move(direction,delta)
+
+func set_new_movement():
+	if direction.length() == 0:
+		direction = Vector2(rand_range(-1,1),rand_range(-1,1)).normalized()
+		duration = randi() % 5
+	else:
+		direction = Vector2(0,0)
+		duration = randi() % 3
+		get_node("Sprite").start_idle()
+	
+	acting_since = 0
--- MainScene.gd
+++ MainScene.gd
@@ -1,9 +1,20 @@
 extends Node
 
+var mob_factory = load("res://IA/IA.tscn")
+
 onready var alice = get_node("Alice")
 onready var charlie = get_node("Charlie")
 onready var bob = get_node("Bob")
 onready var erin = get_node("Erin")
+
+var width = Globals.get("display/width")
+var height = Globals.get("display/height")
+var center = Vector2(width / 2, height / 2)
+
+var last_spawn = 0
+var spawn_interval = 3
+
+var mobs = Array()
 
 func _ready():
 	alice.controller = 0
@@ -19,3 +30,21 @@ func _ready():
 	erin.joystick_side = 1
 	
 	set_process(true)
+
+func _process(delta):
+	last_spawn += delta
+	
+	if last_spawn > spawn_interval:
+		var mob = mob_factory.instance()
+		mob.set_pos(center)
+		self.add_child(mob)
+		
+		mobs.append(mob)
+		
+		last_spawn = 0
+	
+	for mob in mobs:
+		var mob_pos = mob.get_pos()
+		
+		if mob_pos.x < -10 || mob_pos.x > width + 10 || mob_pos.y < -10 || mob_pos.y > height + 10:
+			mob.free()
