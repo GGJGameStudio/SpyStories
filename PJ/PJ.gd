cff20c05db3f22c3330ff814bb604fff839d2ca9
ANJI-DESKTOP\flori <florian.harreau@outlook.com>
2018-01-28 17:19:43 +0100

    reverse

--- PJ/PJ.gd
+++ PJ/PJ.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-var controller
-var joystick_side
 var deadzone = 0.25
 
 func _ready():
@@ -10,10 +8,10 @@ func _ready():
 func _process(delta):
 	var sprite = self.get_node("Sprite")
 	
-	if joystick_side == 0 && Input.is_joy_button_pressed(controller, 12) || joystick_side == 1 && Input.is_joy_button_pressed(controller, 3):
+	if Input.is_action_pressed("ui_select"):
 		sprite.start_acting()
 	
-	var direction = Vector2(Input.get_joy_axis(controller,2*joystick_side), Input.get_joy_axis(controller,2*joystick_side+1))
+	var direction = Vector2(Input.get_joy_axis(0,0), Input.get_joy_axis(0,1))
 	
 	if abs(direction.x) > deadzone || abs(direction.y) > deadzone:
 		sprite.move(direction.normalized(), delta)
