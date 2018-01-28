b5609d165374317b771c224156db4a598737c0a7
Laora Heintz <lola.mitou@gmail.com>
2018-01-28 15:34:03 +0100

    sounds

--- Goals/Goal.gd
+++ Goals/Goal.gd
@@ -27,6 +27,7 @@ func _on_Area2D_body_enter( body ):
 	var package = (pj == global.owner)
 	if name == "PostBox" && package && pj.is_in_group("cia_could_win"):
 		global.winner = "cia"
+		get_node("InPostBox").play("courrier_depose")
 		get_tree().change_scene("res://Screen/End.tscn")
 	if (name=="Exit1" || name=="Exit2") && package && (pj.is_in_group("cia_could_win") || pj.is_in_group("kgb_could_win")):
 		if pj.is_in_group("cia_could_win") : 
