b7febc23e05ca79d413bcd72fa630cce99e83e48
ANJI-DESKTOP\flori <florian.harreau@outlook.com>
2018-01-28 02:24:35 +0100

    fix movement

--- Sprites/BuddySprite.gd
+++ Sprites/BuddySprite.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-enum Action {
-	Idle,
-	WalkingUp,
-	WalkingDown,
-	WalkingRight,
-	WalkingLeft,
-	Acting,
-	Paralyzed
+enum Direction {
+	Up,
+	Down,
+	Right,
+	Left
 }
 
 const medium = sqrt(2)/2
 
 var templatedSprites = [load("res://Sprites/NakedDudeSprite.tscn"),load("res://Sprites/Guy_1.tscn")]
 
-var last_move = Action.Idle
+var is_acting = false
+var is_idle = true
+var last_direction = Direction.Down
+
 var acting_since = 0
 var velocity = 50
 var sprite
@@ -29,11 +29,11 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	if last_move == Action.Acting:
+	if is_acting:
 		acting_since += delta
 	
 	if acting_since > 0.5:
-		last_move = Action.Idle
+		is_acting = false
 		acting_since = 0
 		self.start_idle()
 
@@ -48,67 +48,66 @@ func set_is_running(is_running):
 	velocity = 100 if is_running else 50
 
 func start_idle():
-	if last_move != Action.Acting:
-		if last_move == Action.WalkingUp:
+	if !is_acting:
+		is_idle = true
+		
+		if last_direction == Direction.Up:
 			sprite.set_animation("idle_back")
-		elif last_move == Action.WalkingDown || last_move == Action.Idle || last_move == Action.Acting: 
+		elif last_direction == Direction.Down:
 			sprite.set_animation("idle_front")
-		elif last_move == Action.WalkingRight:
+		elif last_direction == Direction.Right:
 			sprite.set_animation("idle_right")
-		elif last_move == Action.WalkingLeft:
+		elif last_direction == Direction.Left:
 			sprite.set_animation("idle_left")
-		last_move = Action.Idle
 
 func start_acting():
-	if last_move != Action.Acting:
-		if last_move == Action.WalkingUp:
+	if !is_acting:
+		is_acting = true
+		
+		if last_direction == Direction.Up:
 			sprite.set_animation("action_back")
-		elif last_move == Action.WalkingDown || last_move == Action.Idle:
+		elif last_direction == Direction.Down:
 			sprite.set_animation("action_front")
-		elif last_move == Action.WalkingRight:
+		elif last_direction == Direction.Right:
 			sprite.set_animation("action_right")
-		elif last_move == Action.WalkingLeft:
+		elif last_direction == Direction.Left:
 			sprite.set_animation("action_left")
-		last_move = Action.Acting
 
 func start_paralysis():
-	if last_move == Action.WalkingUp:
+	if last_direction == Direction.Up:
 		sprite.set_animation("falling_back")
-	elif last_move == Action.WalkingDown || last_move == Action.Idle || last_move == Action.Acting: 
+	elif last_direction == Direction.Down: 
 		sprite.set_animation("falling_front")
-	elif last_move == Action.WalkingRight:
+	elif last_direction == Direction.Right:
 		sprite.set_animation("falling_right")
-	elif last_move == Action.WalkingLeft:
+	elif last_direction == Direction.Left:
 		sprite.set_animation("falling_left")
 
-func stopped():
-	sprite.set_animation(null)
-	last_move = Action.Paralyzed
-
 func move(direction, delta):
-	if last_move != Action.Acting:
-		var current_move
+	if !is_acting:
+		var current_direction
 		
 		if abs(direction.x) < medium && direction.y < 0:
-			current_move = Action.WalkingUp
+			current_direction = Direction.Up
 		elif abs(direction.x) < medium && direction.y > 0:
-			current_move = Action.WalkingDown
+			current_direction = Direction.Down
 		elif abs(direction.x) >= medium && direction.x > 0:
-			current_move = Action.WalkingRight
+			current_direction = Direction.Right
 		elif abs(direction.x) >= medium && direction.x < 0:
-			current_move = Action.WalkingLeft
+			current_direction = Direction.Left
 	
-		if current_move != last_move:
-			if current_move == Action.WalkingUp:
+		if current_direction != last_direction || is_idle:
+			if current_direction == Direction.Up:
 				sprite.set_animation("walk_up")
-			elif current_move == Action.WalkingDown:
+			elif current_direction == Direction.Down:
 				sprite.set_animation("walk_down")
-			elif current_move == Action.WalkingRight:
+			elif current_direction == Direction.Right:
 				sprite.set_animation("walk_right")
-			elif current_move == Action.WalkingLeft:
+			elif current_direction == Direction.Left:
 				sprite.set_animation("walk_left")
 		
 		var current_pos = self.get_global_pos()
 		get_parent().set_pos(current_pos + direction * velocity * delta)
+		is_idle = false
 		
-		last_move = current_move
+		last_direction = current_direction
